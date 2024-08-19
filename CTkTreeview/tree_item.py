b477ed788ae8098b849a0906c551a1924b2d80b0
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-08-19 11:22:06 -0500

    [UPDATE] Make TreeItem iterable & searchable

--- CTkTreeview/tree_item.py
+++ CTkTreeview/tree_item.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
+from collections.abc import Iterator
 from tkinter import ttk
 from typing import TYPE_CHECKING
+import queue
 
 from attrs import define, field
 from icecream import ic
 import customtkinter as ctk
 
 if TYPE_CHECKING:
-    from typing import Any, Optional, ClassVar
+    from typing import Any, Optional, ClassVar, TypeAlias
 
 @define
 class TreeColumn:
@@ -42,9 +44,12 @@ class TreeColumn:
 class TreeItem:
     next_id: ClassVar = 0
     nodes: list[TreeNode] = field(factory=list, init=False)
-    items: dict[int, TreeItem] = field(factory=dict, init=False)
-    parent: Optional[TreeItem] = None
+    items: list[TreeItem] = field(factory=list, init=False)
+    parent: Optional[TreeItem] = field(init=False, default=None)
     iid: str = ""
+
+    def __iter__(self):
+        return TreeItemIterator(self)
 
     def __attrs_post_init__(self):
         if not self.iid:
@@ -56,12 +61,50 @@ class TreeItem:
         node.parent = self
 
     def add_item(self, item: TreeItem):
-        end = len(self.items)
-        self.items[end] = item
+        self.items.append(item)
+        item.parent = self
+
+    def find(self, iid: str) -> TreeItem | None:
+        res = None
+        for item in self.items:
+            if item.iid == iid:
+                res = item
+            elif item.items:
+                res = item.find(iid)
+
+            if res is not None:
+                break
+
+        return res
 
     @property
     def is_root(self) -> bool:
         return self.parent is None
+
+class TreeItemIterator(Iterator[TreeItem]):
+    StackType: TypeAlias = queue.SimpleQueue[TreeItem]
+
+    def __init__(self, item: TreeItem):
+        self.item = item
+        self.x = 0
+        self.stack = self._make_stack()
+
+    def _make_stack_recurse(self, stack: StackType, item: TreeItem):
+        for item in item.items:
+            stack.put_nowait(item)
+            if len(item.items) > 0:
+                self._make_stack_recurse(stack, item)
+
+    def _make_stack(self):
+        stack = self.StackType()
+        self._make_stack_recurse(stack, self.item)
+        return stack
+
+    def __next__(self) -> TreeItem:
+        try:
+            return self.stack.get_nowait()
+        except queue.Empty:
+            raise StopIteration
 
 @define
 class TreeNode:
