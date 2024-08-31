3fba1f32be1d8ad54c0820cb8ddf2c9ca737446d
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-08-30 20:17:41 -0500

    [FEATURE] Treeview: Context managers for headings/columns
    Implements context managers for setting column and
    heading options.

--- CTkTreeview/treeview.py
+++ CTkTreeview/treeview.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
+from contextlib import AbstractContextManager
 from tkinter import Grid, Pack, Place, ttk
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING, cast, overload
 import re
+from typing_extensions import reveal_type
 
 from icecream import ic
 import customtkinter as ctk
@@ -9,9 +11,107 @@ import customtkinter as ctk
 from .utils import grid
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
+    from collections.abc import Callable, Iterable
     from typing import Any, Literal
-    from .types import Color
+    from typing_extensions import Self
+    from .types import Anchor, Color, ImageSpec
+
+class _HeadingsContextManager(AbstractContextManager):
+    def __init__(self, obj: CTkTreeview) -> None:
+        self.obj = obj
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        pass
+
+    def anchor(self, column: str | int, anchor: Anchor) -> None:
+        """
+        Set the anchor argument of column.
+        """
+        self.obj.heading(column, anchor=anchor)
+
+    def command(self, column: str | int, command: Callable[[], None]) -> None:
+        self.obj.heading(column, command=command)
+
+    def image(self, column: str | int, image: ImageSpec) -> None:
+        self.obj.heading(column, image=image)
+
+    def text(self, column: str | int, text: str) -> None:
+        self.obj.heading(column, text=text)
+
+class _ColumnContextManager(AbstractContextManager):
+    def __init__(self, obj: CTkTreeview) -> None:
+        self.obj = obj
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        pass
+
+    @overload
+    def anchor(self, column: int | str, anchor: Anchor) -> None:
+        ...
+
+    @overload
+    def anchor(self, column: int | str) -> Anchor:
+        ...
+
+    def anchor(self, column: int | str, anchor=None):
+        if anchor is not None:
+            self.obj.column(column, anchor=anchor)
+            return
+
+        return cast("Anchor", self.obj.column(column, 'anchor'))
+
+    def id(self, column: int | str) -> str:
+        return self.obj.column(column, 'id')
+
+    @overload
+    def minwidth(self, column: int | str) -> int:
+        ...
+
+    @overload
+    def minwidth(self, column: int | str, minwidth: int) -> None:
+        ...
+
+    def minwidth(self, column: int | str, minwidth=None):
+        if minwidth is not None:
+            self.obj.column(column, minwidth=minwidth)
+            return
+
+        return self.obj.column(column, 'minwidth')
+
+    @overload
+    def width(self, column: int | str) -> int:
+        ...
+
+    @overload
+    def width(self, column: int | str, width: int) -> None:
+        ...
+
+    def width(self, column: int | str, width=None):
+        if width is not None:
+            self.obj.column(column, width=width)
+            return
+
+        return self.obj.column(column, 'width')
+
+    @overload
+    def stretch(self, column: int | str, stretch: bool) -> None:
+        ...
+
+    @overload
+    def stretch(self, column: int | str) -> bool:
+        ...
+
+    def stretch(self, column: int | str, stretch=None):
+        if stretch is not None:
+            self.obj.column(column, stretch=stretch)
+            return
+        return self.obj.column(column, 'stretch')
 
 class CTkTreeview(ttk.Treeview):
     def __init__(
@@ -57,3 +157,9 @@ class CTkTreeview(ttk.Treeview):
                 setattr(self, m, getattr(self.frame, m))
 
         # TODO: Bind double-click method
+
+    def columns(self):
+        return _ColumnContextManager(self)
+
+    def headings(self):
+        return _HeadingsContextManager(self)
