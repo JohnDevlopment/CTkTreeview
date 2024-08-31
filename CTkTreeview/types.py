883eaf900a740b360dd9eaf6555b5e05a30cb71d
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-08-30 20:16:48 -0500

    [REFACTOR] Types: Anchor & ImageSpec are aliases

--- CTkTreeview/types.py
+++ CTkTreeview/types.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TypedDict
+import tkinter as _tk
 
 if TYPE_CHECKING:
-    from typing import TypeAlias
+    from collections.abc import Callable
+    from typing import Any, TypeAlias
 
+Anchor: TypeAlias = _tk._Anchor
 Color: TypeAlias = str | tuple[str, str]
+ImageSpec: TypeAlias = _tk._ImageSpec
 ScreenUnits: TypeAlias = str | float
 Padding: TypeAlias = (
     ScreenUnits
