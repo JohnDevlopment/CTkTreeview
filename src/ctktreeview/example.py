3383df2a1674a5dbf4b8678fca84a46b474703d6
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2026-07-03 22:25:07 -0500

    [FORMAT] src/ctktreeview/example.py

--- src/ctktreeview/example.py
+++ src/ctktreeview/example.py
@@ -1,16 +1,10 @@
 from __future__ import annotations
-from datetime import datetime
-from tkinter import ttk
 
-from icecream import ic
 import customtkinter as ctk
 
 from .treeview import CTkTreeview
 from .utils import grid
 
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from typing import Optional
 
 def main():
     app = ctk.CTk()
@@ -28,5 +22,6 @@ def main():
 
     app.mainloop()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
