cc1d70b2bf2c618241ee70aeed338efda4bf35af
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2026-07-04 00:59:41 -0500

    [FORMAT] src/ctktreeview/typings.py

--- src/ctktreeview/typings.py
+++ src/ctktreeview/typings.py
@@ -1,41 +1,30 @@
-"""
-Type aliases for use with other modules.
-"""
 from __future__ import annotations
+
 from _tkinter import Tcl_Obj
-from tkinter import Image
+from tkinter import Image, Misc
 from tkinter.font import Font
-from typing import TYPE_CHECKING, Any, Literal
+from typing import TYPE_CHECKING, Any, Literal, TypedDict, type_check_only
 
 if TYPE_CHECKING:
     from typing import TypeAlias
 
-#: For ``anchor=`` arguments in methods. The following values
-#: are valid:
-#: * nw (northwest)
-#: * n (north)
-#: * ne (northeast)
-#: * w (west)
-#: * center
-#: * e (east)
-#: * sw (southwest)
-#: * s (south)
-#: * se (southeast)
-Anchor: TypeAlias = Literal['nw', 'n', 'ne', 'w', 'center', 'e', 'sw', 's', 'se']
+Anchor: TypeAlias = Literal["nw", "n", "ne", "w", "center", "e", "sw", "s", "se"]
 
-#: Color description. The tuple version describes the colors
-#: for light mode and dark mode; in the case of a single
-#: string, both light and dark mode.
 Color: TypeAlias = str | tuple[str, str]
 
-#: For ``font=`` arguments in methods.
-FontDescription: TypeAlias = (str | Font | list[Any] | tuple[str] | tuple[str, int] |
-    tuple[str, int, str] | tuple[str, int, list[str] | tuple[str, ...]] | Tcl_Obj)
+FontDescription: TypeAlias = (
+    str
+    | Font
+    | list[Any]
+    | tuple[str]
+    | tuple[str, int]
+    | tuple[str, int, str]
+    | tuple[str, int, list[str] | tuple[str, ...]]
+    | Tcl_Obj
+)
 
-#: For ``image=`` arguments in methods.
 ImageSpec: TypeAlias = Image | str
 
-#: Screen units (i.e., )
 ScreenUnits: TypeAlias = str | float
 
 Padding: TypeAlias = (
